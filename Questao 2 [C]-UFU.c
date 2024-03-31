0d7fd22dc3d47b05c47e3384ba4713bd63428ec0
luxxaa12 <159189844+luxxaa12@users.noreply.github.com>
2024-03-31 18:24:13 -0300

    Add files via upload

---
+++ Questao 2 [C]-UFU.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+
+int main (){
+	
+	int VT[6];
+	int i;
+	
+	for(i=0;i<6;i++){
+	printf("Digite o valor: %d: ",i+1);
+	scanf("%dª",&VT[i]);	
+}
+	for(i=0;i<6;i++){
+	printf("\nVT %d: %d\n",i+1,VT[i]);
+	
+	}	
+	
+	return 0;	
+	}
+
+	
+
