e1f62b7d69b6c12c0eb2ddad71dabce3b53233cc
luxxaa12 <159189844+luxxaa12@users.noreply.github.com>
2024-04-01 00:06:29 -0300

    Add files via upload

---
+++ Questao 3 [C]-UFU.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+int main(){
+	float NR[10];
+	int i;
+	float quadrado[10];
+	
+	for(i=0;i<10;i++){
+	printf("Digite o numero real: %d:",i+1);
+	scanf("%f",&NR[i]);
+	}
+		quadrado[10] =NR[i]*NR[i];
+	
+	/*for(i=0;i<10;i++){
+	printf("\nNR %d: %.1f\n",i+1,NR[i]);
+	}*/
+	for(i=0;i<10;i++){
+	printf("\nExibindo raiz quadrada dos vetores %d %.1f\n",i+1,quadrado[i]);
+	}
+	return 0;
+}
